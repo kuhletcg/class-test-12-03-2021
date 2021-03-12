ab5d5d1c9d833dca47b8f334918d991a3fdf58fd
TCG <ssibonokuhle@gmail.com>
2021-03-12 11:16:16 +0200

    Added a solution of array

--- index.js
+++ index.js
@@ -3,10 +3,3 @@ var order = [];
 for (var i = 0; i < numbers.length; i++) {
   order.push(numbers[i]);
 }
-
-var numArray = [24, 8, 23, 32, 5, 62];
-numArray.sort(function (a, b) {
-  return a - b;
-});
-
-console.log(numArray);
